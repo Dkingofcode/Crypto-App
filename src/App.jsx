1089c3f3e1fe83ef2a9fb5694321856dab089c6d
David <doladepo128@gmail.com>
2023-08-14 19:08:54 +0100

    creating the file structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,37 @@
 import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import { Layout, Typography, Space } from 'antd';
+import { Navbar, Exchanges, Homepage, Cryptocurrencies, News, CryptoDetails } from './components';
 import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0);
+  
 
   return (
-    <>
-      <div>
-        <h1>Cryptoverse</h1>
+    <div className='app'>
+      <div className='navbar'>
+        <Navbar  />
+        </div>
+        <div className='main'>
+         <Layout>
+          <div>
+           <BrowserRouter>
+             <Routes>
+              <Route path='/*'  element={<Homepage />} />
+              <Route path='/exchanges' element={<Exchanges />}  />
+              <Route path="/cryptocurrencies" element={<Cryptocurrencies />} />
+              {/* <Route path="/crypto/:coinId" element={<CryptoDetails  />}   /> */}
+              <Route path="/news" element={<News  />} />
+             </Routes>
+           </BrowserRouter>
+            </div>
+          </Layout>  
+        </div>
+
+        <div className='footer'>
+
+        </div>
       </div>
-    </>
   )
 }
 
---
+++ src/components/index.js
@@ -0,0 +1,20 @@
+import React from 'react';
+
+import CryptoDetails from './CryptoDetails';
+import Cryptocurrencies from './Cryptocurrencies';
+import News from './News';
+import Homepage from './Homepage';
+import Exchanges from './Exchanges';
+import Navbar from './Navbar';
+
+export { CryptoDetails, Cryptocurrencies, Navbar, Exchanges, News, Homepage}
+
+
+
+
+
+
+
+
+
+
